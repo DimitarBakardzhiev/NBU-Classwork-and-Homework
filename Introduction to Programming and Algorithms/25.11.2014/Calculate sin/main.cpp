7e7b45f9dc434d1608edd54e475742a9592906ab
Rossoner40 <dimitar.bakardzhiev94@gmail.com>
2014-12-02 01:32:08 +0200

    Calculate Sin(X) modified
    Unused code removed. Still doesn't work with large factorials. Factorials and powers are now calculated by reusing the previous value.

--- Introduction to Programming and Algorithms/25.11.2014/Calculate sin/main.cpp
+++ Introduction to Programming and Algorithms/25.11.2014/Calculate sin/main.cpp
@@ -3,59 +3,27 @@
 
 using namespace std;
 
-double Fact(int number)
-{
-    if(number == 0 && number == 1)
-    {
-        return 1;
-    }
-    else
-    {
-        double result = 1;
-        for(int i = 2; i <= number; i++)
-        {
-            result *= i;
-        }
-
-        return result;
-    }
-}
-
-double lastFact = 1;
-
-double EfficientFact(int number, int lastCalculatedFact)
-{
-    if(number == 0 && number == 1)
-    {
-        return 1;
-    }
-    else
-    {
-        for(int i = lastCalculatedFact + 1; i <= number; i++)
-        {
-            lastFact *= i;
-        }
-
-        return lastFact;
-    }
-}
-
 int main()
 {
-    double sin = 0;
-    int sign = 1;
     double x;
     cin >> x;
-    int lastCalculated = 1;
-    for(int i = 1; i < 1000; i += 2)
+    double sin = 0;
+
+    double numerator = x;
+    long long denominator = 1;
+    int sign = 1;
+    for (int i = 1; i <= 66; i += 2)
     {
-        sin += pow(x, i) / EfficientFact(i, lastCalculated) * sign;
-        sign *= -1;
-        if(i > 1)
+        if (i != 1)
         {
-            lastCalculated += 2;
+            numerator *= x * x;
+            denominator *= (i - 1) * i;
         }
+
+        sin += (numerator / denominator) * sign;
+        sign *= -1;
     }
+
 
     cout << sin << endl;
 
