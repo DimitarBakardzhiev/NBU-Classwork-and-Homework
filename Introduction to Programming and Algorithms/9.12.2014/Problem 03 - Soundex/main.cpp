b4a7d02d3db1543258e6fde6b738f53a8e1cd778
Rossoner40 <dimitar.bakardzhiev94@gmail.com>
2014-12-24 15:24:03 +0200

    Added some basics for the soundex algorithm

--- Introduction to Programming and Algorithms/9.12.2014/Problem 03 - Soundex/main.cpp
+++ Introduction to Programming and Algorithms/9.12.2014/Problem 03 - Soundex/main.cpp
@@ -2,8 +2,41 @@
 
 using namespace std;
 
+const char vowels[9] = { 'A', 'E', 'I', 'O', 'U', 'H', 'W', 'Y' };
+
+int consonantToCode(char consonant)
+{
+    if (consonant == 'B' || consonant == 'F'
+        || consonant == 'P' || consonant == 'V')
+    {
+        return 1;
+    }
+    else if (consonant == 'C' || consonant == 'G' || consonant == 'J'
+        || consonant == 'K' || consonant == 'Q' || consonant == 'S'
+        || consonant == 'X' || consonant == 'Z')
+    {
+        return 2;
+    }
+    else if (consonant == 'D' || consonant == 'T')
+    {
+        return 3;
+    }
+    else if (consonant == 'L')
+    {
+        return 4;
+    }
+    else if (consonant == 'M' || consonant == 'N')
+    {
+        return 5;
+    }
+    else
+    {
+        return 6;
+    }
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+
     return 0;
 }
