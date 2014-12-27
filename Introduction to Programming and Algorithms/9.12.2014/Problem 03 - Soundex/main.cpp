160bcc2fe397e7d327a8c690e6f1303961958981
Rossoner40 <dimitar.bakardzhiev94@gmail.com>
2014-12-27 05:37:37 +0200

    Soundex almost finished. TODO: Fix random bugs

--- Introduction to Programming and Algorithms/9.12.2014/Problem 03 - Soundex/main.cpp
+++ Introduction to Programming and Algorithms/9.12.2014/Problem 03 - Soundex/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-const char vowels[9] = { 'A', 'E', 'I', 'O', 'U', 'H', 'W', 'Y' };
+bool isVowel(char character)
+{
+    return character == 'A' || character == 'E' || character == 'I' || character == 'O'
+        || character == 'U' || character == 'H' || character == 'W' || character == 'Y';
+}
 
 int consonantToCode(char consonant)
 {
@@ -35,8 +40,77 @@ int consonantToCode(char consonant)
     }
 }
 
+void toUpperCase(char* text)
+{
+    for (int i = 0; text[i] != '\0'; i++)
+    {
+        int asciiCode = (int)text[i];
+        if (asciiCode >= 97 && asciiCode <= 122)
+        {
+            int upperCharCode = asciiCode - 32;
+            text[i] = (char)upperCharCode;
+        }
+    }
+}
+
+void fillWithZeroes(char* soundexCode)
+{
+    for (int i = 1; i < 4; i++)
+    {
+        if (soundexCode[i] == '\0')
+        {
+            soundexCode[i] = (char)48;
+        }
+    }
+}
+
+char* soundex(char* name)
+{
+    //toUpperCase(name);
+
+    char result[5] = { '\0' };
+    char lastConsonant = '\0';
+    for (int i = 0, j = 0; name[i] != '\0'; i++)
+    {
+        if (i == 0)
+        {
+            result[j] = name[i];
+            j++;
+            lastConsonant = name[i];
+        }
+        else
+        {
+            if (name[i] == lastConsonant || isVowel(name[i]))
+            {
+                continue;
+            }
+            else if (j > 3)
+            {
+                break;
+            }
+            else
+            {
+                int code = consonantToCode(name[i]);
+                result[j] = (char)(code + '\0');
+                j++;
+                lastConsonant = name[i];
+            }
+        }
+    }
+
+    fillWithZeroes(result);
+
+    return result;
+}
+
 int main()
 {
+    char code[5];
+    strcpy(code, soundex("ROBERT"));
+    for (int i = 0; i < 4; i++)
+    {
+        cout << code[i];
+    }
 
     return 0;
 }
