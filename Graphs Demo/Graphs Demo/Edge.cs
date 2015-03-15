4797fd84ae6967d8be5382d87084d91a652dfb3c
Rossoner40 <dimitar.bakardzhiev94@gmail.com>
2015-03-15 17:48:01 +0200

    Graphs Demo for Introduction to Informatics course added.

---
+++ Graphs Demo/Graphs Demo/Edge.cs
@@ -0,0 +1,35 @@
+﻿namespace Graphs_Demo
+{
+    using System;
+
+    public class Edge
+    {
+        private Node target;
+
+        public Edge(Node target, int weight)
+        {
+            this.Target = target;
+            this.Weight = weight;
+        }
+
+        public Node Target
+        {
+            get
+            {
+                return this.target;
+            }
+
+            private set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Target cannot be set to null!");
+                }
+
+                this.target = value;
+            }
+        }
+
+        public int Weight { get; private set; }
+    }
+}
---
+++ Graphs Demo/Graphs Demo/Graph.cs
@@ -0,0 +1,19 @@
+﻿namespace Graphs_Demo
+{
+    using System.Collections.Generic;
+
+    public class Graph
+    {
+        public Graph()
+        {
+            this.Nodes = new Dictionary<string, Node>();
+        }
+
+        public IDictionary<string, Node> Nodes { get; private set; }
+
+        public void AddNode(Node node)
+        {
+            this.Nodes[node.Name] = node;
+        }
+    }
+}
